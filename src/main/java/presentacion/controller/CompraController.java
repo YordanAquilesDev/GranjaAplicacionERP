791841e3bfc8e1be326774c2a33244d447a2558a
Administrador <Administrador@LN-LCA0605-10.grupoutp.net>
2026-07-09 15:17:43 -0500

    integrando logica  conexion de UI con Logica

--- src/main/java/presentacion/controller/CompraController.java
+++ src/main/java/presentacion/controller/CompraController.java
@@ -1,11 +1,32 @@
 package presentacion.controller;
 
+import dominio.modelos.Producto;
+import dominio.modelos.Proveedor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class CompraController {
 
+  /**
+    * -------------------------------------------------
+    * DECLARACION DE VARIABLES
+    * -------------------------------------------------
+    */
+    private Label txtCantidad;
+    private Label txtCostoUnitario;
+    private DatePicker dpFecha;
+    private ComboBox<Proveedor> cmbProveedor;
 
+    private ComboBox<Producto> cmbProducto;
+
+    
+    
 @FXML
 public Label txtIdCompra;
 
@@ -25,10 +46,16 @@ public Label txtIdCompra;
     }
     @FXML
     private void handleSeleccionarFila(MouseEvent even){
+        Proveedor provee= new Proveedor();
+        Producto produ= new Producto();
         txtIdCompra.setText("");
-        cmbProveedor
-        dpFecha
-        cmbProducto
+        
+         ObservableList<Proveedor> proveedor= FXCollections.observableArrayList(provee);
+         ObservableList<Producto> producto= FXCollections.observableArrayList(produ);
+          
+        cmbProveedor.setItems(proveedor);
+        //dpFecha.
+        cmbProducto.setItems(producto);
         txtCantidad.setText("");
         txtCostoUnitario.setText("");
 
--- src/main/java/presentacion/controller/VentaController.java
+++ src/main/java/presentacion/controller/VentaController.java
@@ -1,9 +1,43 @@
 package presentacion.controller;
 
+import aplicacion.serviceimpl.ClienteService;
+import aplicacion.serviceimpl.ProductoServiceImpl;
+import dominio.modelos.Cliente;
+import dominio.modelos.DetalleVenta;
+import dominio.modelos.Producto;
+import dominio.servicio.JlaService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class VentaController {
+    
+   /**
+    * -------------------------------------------------
+    * DECLARACION DE VARIABLES
+    * -------------------------------------------------
+    */
+    private final JlaService clienteService;
+    private final JlaService  productoService;
+    private ComboBox<Producto> cmbProducto;
+    private Label txtCantidad;
+    private Label txtPrecioUnitario;
+    private TableView<DetalleVenta> tblDetalleVenta; 
+    /**
+     * -----------------------------------------------
+     * CONSTRUCTORES    
+     * ----------------------------------------------
+     */
+    
+    public VentaController(){
+        this.clienteService= new ClienteService();
+        this.productoService= new ProductoServiceImpl();
+    }
 
     @FXML
     private void handleLimpiarTodo(ActionEvent event) {
@@ -26,8 +60,12 @@ public class VentaController {
     }
     @FXML
     private void handleSeleccionarFila(MouseEvent  event){
-      cmbProducto
-      txtCantidad
-      txtPrecioUnitario
+       DetalleVenta detalle= tblDetalleVenta.getSelectionModel().getSelectedItem();
+       ObservableList<Producto> producto= FXCollections.observableArrayList(productoService.findAll());
+       ObservableList<Cliente> clientes=FXCollections.observableArrayList(clienteService.findAll());
+      cmbProducto.setItems(producto);
+      txtCantidad.setText(String.valueOf(detalle.getCantidad()));
+      txtPrecioUnitario.setText(String.valueOf(detalle.getProducto().getPrecio()));
+      
     }
 }
